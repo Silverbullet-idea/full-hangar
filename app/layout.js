40f558da03b38252fd7b818d67c955d64e537395
Silverbullet-idea <rdale68@gmail.com>
2026-02-28 11:54:13 -0800

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Full-Hangar',
+  description: 'Aircraft Market Intelligence & Deal-Finding Platform',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
